7a725748b547664e2e8bac8f225c085a20cc2bf5
faishalmanzar <faishalmanzar@gmail.com>
2024-02-21 23:55:36 +0530

    Added matrix maths

--- SourceFile/main.cpp
+++ SourceFile/main.cpp
@@ -13,12 +13,20 @@ int main() {
   glGenVertexArrays(1, &vao);
   glBindVertexArray(vao);
 
-  vec3 a(1.0f, 2.0f, 5.0f);
-  vec3 b(2, 4, 3);
-  vec3 c = a + b;
+  // vec3 a(1.0f, 2.0f, 5.0f);
+  // vec3 b(2, 4, 3);
+  // vec3 c = a + b;
+
+  mat4 position = mat4::translation(vec3(2, 3, 4));
+  position *= mat4::identity();
+
+  position.elements[12] = 2.0f;
+  vec4 column = position.columns[3];
+  std::cout << column << std::endl;
+
   while (!window.closed()) {
     window.clear();
-    std::cout << c << " " << a << std::endl;
+    // std::cout << c << " " << a << std::endl;
     glBegin(GL_QUADS);
     glVertex2f(-0.5f, -0.5f);
     glVertex2f(-0.5f, 0.5f);
