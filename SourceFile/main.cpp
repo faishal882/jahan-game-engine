e8b2ad24f1e83a0c8cdd4d0f9e3270ff111f152d
faishalmanzar <faishalmanzar@gmail.com>
2024-02-22 16:40:25 +0530

    Added Shaders

--- SourceFile/main.cpp
+++ SourceFile/main.cpp
@@ -1,3 +1,4 @@
+#include "src/graphics/shader.h"
 #include "src/graphics/window.h"
 #include "src/maths/maths.h"
 
@@ -7,32 +8,31 @@ int main() {
   using namespace maths;
 
   Window window("Sparky!", 960, 540);
-  glClearColor(0.2f, 0.3f, 0.8f, 1.0f);
+  glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
 
-  GLuint vao;
-  glGenVertexArrays(1, &vao);
-  glBindVertexArray(vao);
+  GLfloat vertices[] = {4, 3, 0, 12, 3, 0, 4,  6, 0,
+                        4, 6, 0, 12, 6, 0, 12, 3, 0};
 
-  // vec3 a(1.0f, 2.0f, 5.0f);
-  // vec3 b(2, 4, 3);
-  // vec3 c = a + b;
+  GLuint vbo;
+  glGenBuffers(1, &vbo);
+  glBindBuffer(GL_ARRAY_BUFFER, vbo);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+  glEnableVertexAttribArray(0);
 
-  mat4 position = mat4::translation(vec3(2, 3, 4));
-  position *= mat4::identity();
+  mat4 ortho = mat4::orthographic(0.0f, 16.0f, 0.0f, 9.0f, -1.0f, 1.0f);
 
-  position.elements[12] = 2.0f;
-  vec4 column = position.columns[3];
-  std::cout << column << std::endl;
+  Shader shader("src/shaders/basic.vert", "src/shaders/basic.frag");
+  shader.enbale();
+  shader.setUniformMat4("pr_matrix", ortho);
+  shader.setUniformMat4("ml_matrix", mat4::rotation(45.0f, vec3(0, 0, 1)));
+
+  // shader.setUniform2f("light_pos", vec2(8.0f, 4.5f));
+  shader.setUniform4f("colour", vec4(0.2f, 0.3f, 0.8f, 1.0f));
 
   while (!window.closed()) {
     window.clear();
-    // std::cout << c << " " << a << std::endl;
-    glBegin(GL_QUADS);
-    glVertex2f(-0.5f, -0.5f);
-    glVertex2f(-0.5f, 0.5f);
-    glVertex2f(0.5f, 0.5f);
-    glVertex2f(0.5f, -0.5f);
-    glEnd();
+    glDrawArrays(GL_TRIANGLES, 0, 6);
     window.update();
   }
 
